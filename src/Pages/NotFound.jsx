ee5a94469c5eb963688188b039414e907d032670
John Newcomb <jnewcomb125@gmail.com>
2021-03-31 23:54:14 -0700

    Adding changes to get new update

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <h1 className='is-size-1 mb-5'>Page not found</h1>
+    <h1 className='is-size-1 mb-5'>
+      Page not found!
+    </h1>
   );
 }
 
